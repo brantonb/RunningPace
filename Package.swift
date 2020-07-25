19a48878624b06002da2e3ac78ed0b948be48734
Branton Boehm <development@brantonboehm.com>
2020-07-25 13:28:46 -0700

    Add typealias for Speed, Distance, and Duration

--- Package.swift
+++ Package.swift
@@ -3,6 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "RunningPace",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .watchOS(.v2),
+        .tvOS(.v9),
+    ],
     products: [
         .library(
             name: "RunningPace",
