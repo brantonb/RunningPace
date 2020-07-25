72fb01c7f3285571e58e51daaccbfa1b0b0f561e
Branton Boehm <development@brantonboehm.com>
2020-07-25 14:28:25 -0700

    Implement pace extensions to UnitSpeed (min/mile & min/km)

---
+++ Sources/RunningPace/UnitSpeedExtensions.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+public extension UnitSpeed {
+    static let minutesPerMile = UnitSpeed(symbol: "\(UnitDuration.minutes.symbol)/\(UnitLength.miles.symbol)",
+                                          converter: UnitConverterInverse(constant: 26.8224))
+
+    static let minutesPerKilometer = UnitSpeed(symbol: "\(UnitDuration.minutes.symbol)/\(UnitLength.kilometers.symbol)",
+                                               converter: UnitConverterInverse(constant: 16.666666666667))
+}
+
+private class UnitConverterInverse: UnitConverter {
+    let constant: Double
+
+    init(constant: Double) {
+        self.constant = constant
+    }
+
+    override func baseUnitValue(fromValue value: Double) -> Double {
+        return constant / value
+    }
+
+    override func value(fromBaseUnitValue baseUnitValue: Double) -> Double {
+        return constant / baseUnitValue
+    }
+}
---
+++ Tests/RunningPaceTests/UnitSpeedExtensionsTests.swift
@@ -0,0 +1,56 @@
+import XCTest
+import RunningPace
+
+final class RunningPaceTests: XCTestCase {
+
+    // MARK: - MPH
+
+    func test0MphisInfinityMinutesPerMile() {
+        assertEqualSpeeds(Double.infinity.minutesPerMile, 0.milesPerHour)
+    }
+
+    func test10MphIs6MinutesPerMile() {
+        assertEqualSpeeds(6.minutesPerMile, 10.milesPerHour)
+    }
+
+    func testNegativeMphIsNegativePace() {
+        assertEqualSpeeds((-6).minutesPerMile, (-10).milesPerHour)
+    }
+
+    func testMinutesPerMileConvertsBack() {
+        let original = (8.5).minutesPerMile
+        let milesPerHour = original.converted(to: .milesPerHour)
+        let final = milesPerHour.converted(to: .minutesPerMile)
+        assertEqualSpeeds(original, final)
+    }
+
+    // MARK: - KPH
+
+    func test0KphisInfinityMinutesPerKilometer() {
+        assertEqualSpeeds(Double.infinity.minutesPerKilometer, 0.kilometersPerHour)
+    }
+
+    func test10KphIs6MinutePerKilometers() {
+        assertEqualSpeeds(6.minutesPerKilometer, 10.kilometersPerHour)
+    }
+
+    func testNegativeKphIsNegativePace() {
+        assertEqualSpeeds((-6).minutesPerKilometer, (-10).kilometersPerHour)
+    }
+
+    func testMinutesPerKilometerConvertsBack() {
+        let original = (8.5).minutesPerMile
+        let milesPerHour = original.converted(to: .milesPerHour)
+        let final = milesPerHour.converted(to: .minutesPerMile)
+        assertEqualSpeeds(original, final)
+    }
+}
+
+private func assertEqualSpeeds(_ expected: Speed, _ actual: Speed, file: StaticString = #file, line: UInt = #line) {
+    let tolerance = 0.00001
+    let expectedMetersPerSecond = expected.converted(to: .metersPerSecond).value
+    let actualMetersPerSecond = actual.converted(to: .metersPerSecond).value
+    if fabs(expectedMetersPerSecond - actualMetersPerSecond) >= tolerance {
+        XCTFail("\(expected) is not equal to \(actual)", file: file, line: line)
+    }
+}
